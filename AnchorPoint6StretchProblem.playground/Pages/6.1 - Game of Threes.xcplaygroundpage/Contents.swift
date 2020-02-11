d47ddfc324c3230c2e50ce4fbd67b15d626c7dff
Devin Singh <notpublic@gmail.com>
2020-02-11 09:40:28 -0700

    6.2 completed

--- AnchorPoint6StretchProblem.playground/Pages/6.1 - Game of Threes.xcplaygroundpage/Contents.swift
+++ AnchorPoint6StretchProblem.playground/Pages/6.1 - Game of Threes.xcplaygroundpage/Contents.swift
@@ -33,7 +33,28 @@ func returnSteps(int: Int) -> [Int:Int] {
     }
     return stepsDict
 }
-print(returnSteps(int: 100).compactMap( { ($0.key, $0.value) }).sorted(by: { $0.0 > $1.0 }))
+returnSteps(int: 100).compactMap( { ($0.key, $0.value) }).sorted(by: { $0.0 > $1.0 })
 
 
 //: [Next](@next)
+
+//: [Previous](@previous)
+/*:
+ # Tuesday Stretch Problem 6.2
+ ## Add Ints
+ 
+ ### Instructions:
+ Create a func called add where the method allows one to pass as many or as few Ints as desired and then adds them together and returns the result.
+ 
+ Example: If I call add(3, 7), it prints out 10. Example: If I call add(3, 7, 2, 4), it prints out 16. Example: If I call add(3, 7, 2, 9, 12, 11), it prints out 44.
+ */
+
+import Foundation
+
+//: [Next](@next)
+
+func sumOf(_ numbers: Int...) -> Int {
+    numbers.reduce(0, +)
+}
+
+sumOf(1, 2, 3, 4)
